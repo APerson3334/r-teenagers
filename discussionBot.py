54fa9846df9538e933ab905f5c0d525ebf2a6287
APerson3334 <michaelmarquardt333@gmail.com>
2018-05-26 17:03:20 -0600

    Expanded to 33 posts, changed r.subreddit to reddit.subreddit

--- discussionBot.py
+++ discussionBot.py
@@ -14,7 +14,7 @@ print ‘logged in bois’
 
 while 1 == 1: #Makes the bot work constantly
 
-    post = randint(1,18) #Generates a random whole number between 1 and 18. Can be expanded for more threads.
+    post = randint(1,33) #Generates a random whole number between 1 and 33. Can be expanded for more threads.
 
     if post == 1:
         reddit.subreddit('teenagers').submit(title="What's your unpopular opinion?", selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
@@ -53,25 +53,71 @@ while 1 == 1: #Makes the bot work constantly
 		reddit.subreddit('teenagers').submit(title="What's something that a lot of people like but you don't care for?", selftext='', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
 		print "submitted What's something that a lot of people like but you don't care for?"
 	else if post == 13:
-		r.subreddit('teenagers').submit(title='What was one good thing and one bad thing that happened today?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		reddit.subreddit('teenagers').submit(title='What was one good thing and one bad thing that happened today?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
 		print 'submitted What was one good thing and one bad thing that happened today?'
 	else if post == 14:
-		r.subreddit('teenagers'),submit(title='Yay or nay thread', selftext="""Comment something and people will respond with yay or nay
+		reddit.subreddit('teenagers'),submit(title='Yay or nay thread', selftext="""Comment something and people will respond with yay or nay
 		
 		^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!'""", flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
 		print 'submitted Yay or nay thread'
 	else if post == 15:
-		r.subreddit('teenagers'),submit(title='What is your worst quality?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		reddit.subreddit('teenagers'),submit(title='What is your worst quality?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
 		print 'submitted What is your worst quality?'
 	else if post == 16:
-		r.subreddit('teenagers').submit(title='Truth thread', selftext='Like truth or dare but without the dares since nobody ever chooses dare anyway', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		reddit.subreddit('teenagers').submit(title='Truth thread', selftext='Like truth or dare but without the dares since nobody ever chooses dare anyway', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
 		print 'submitted Truth thread'
 	else if post == 17:
-		r.subreddit('teenagers').submit(title='Sub dare thread', selftext='', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		reddit.subreddit('teenagers').submit(title='Sub dare thread', selftext='', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
 		print 'submitted Sub dare thread'
 	else if post == 18:
-		r.subreddit('teenagers').submit(title='Rice purity test scores', selftext="""http://www.ricepuritytest.com
+		reddit.subreddit('teenagers').submit(title='Rice purity test scores', selftext='http://www.ricepuritytest.com', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		print 'submitted Rice purity test scores'
+	else if post == 20:
+		reddit.subreddit('teenagers').submit(title='Would you rather thread', selftext='', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		print 'submitted Would you rather thread
+	else if post == 21:
+		reddit.subreddit('teenagers').submit(title="What's your favorite thing you own?", selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print "submitted What's your favorite thing you own?"
+	else if post == 22:
+		reddit.subreddit('teenagers').submit(title='User appreciation thread', selftext="""Comment which users you appreciate
+
+Remember you can only mention three people per comment
+
+^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!""", flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+	else if post == 23:
+		reddit.subreddit('teenagers').submit(title='Introduction thread', selftext="""Introduce yourself!
+
+		^(especially if you're new or haven't used this sub frequently)
 
 		^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!""", flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		print 'submitted Introduction thread'
+	else if post == 24:
+		reddit.subreddit('teenagers').submit(title='What is your greatest fear?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+	else if post == 25:
+		reddit.subreddit('teenagers').submit(title='What was the best part of your day?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted What was the best part of your day?'
+	else if post == 26:
+		reddit.subreddit('teenagers').submit(title='What do you think of a user thread', selftext ='Comment and people will tell you what they think about you', flair_id='6b01166e-6435-11e2-a77a-12313d051e91')
+		print 'submitted What dou you think of a user thread'
+	else if post == 27:
+		reddit.subreddit('teenagers').submit(title='General discussion thread', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted General discussion thread'
+	else if post == 28:
+		reddit.subreddit('teenagers').submit(title='Wyd hyd', selftext = '^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted Wyd hyd'
+	else if post == 29:
+		reddit.subreddit('teenagers').submit(title='Who is your favorite artist/band?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted Who is your favorite artist/band?'
+	else if post == 30:
+		reddit.subreddit('teenagers').submit(title='Why is your best friend your best friend?', selftex ='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted Why is your best friend your best friend?'
+	else if post == 31:
+		reddit.subreddit('teenagers').submit(title='Post your favorite image right now', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted Post your favorite image right now'
+	else if post == 32:
+		reddit.subreddit('teenagers').submit(title='What are you listening to?', selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
+		print 'submitted What are you listening to?'
+	else if post == 33:
+		reddit.subreddit('teenagers').submit(title="What's your favorite song?", selftext='^^remember ^^to ^^sort ^^/r/teenagers ^^by ^^new ^^to ^^engage ^^in ^^new ^^discussion ^^posts!', flair_id='1b713a20-845f-11e3-a1ce-12313d224170')
     # Continue for the rest of the threads in timawesomeness' wiki
-    time.sleep(900) # Wait 900 seconds (15 minutes) before posting again
+    time.sleep(3600) # Wait 3600 seconds (1 hour) before posting again
